c0046f423cb847efe0821c5ce7fce51f62133d4d
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-06-20 10:43:30 +0300

    swap with variable

--- task2.py
+++ task2.py
@@ -1,10 +1,10 @@
+#input side of triangle
 import math
 
-#calculate square of triangle
 def calculate_triangle_aquare ():
-    a = int(input("input side a: "))
-    b = int(input("input side b: "))
-    c = int(input("input side c: "))
+    a = float(input("input side a: "))
+    b = float(input("input side b: "))
+    c = float(input("input side c: "))
     p = 0.5*(a+b+c)
     return math.sqrt(p*(p-a)*(p-b)*(p-c))
 
---
+++ task3.py
@@ -0,0 +1,9 @@
+#swap variables with tmp variable
+a = 5
+b = 10
+tmp = a
+a = b
+b = tmp
+
+print(a)
+print(b)

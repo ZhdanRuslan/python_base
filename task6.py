3d38493266a35fa281902fb1721866b4fcbc4ad1
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-06-20 17:35:42 +0300

    add validation of wrong input

--- task6.py
+++ task6.py
@@ -1,10 +1,13 @@
 #listen input and check the number
+import os
 while True:
     print("Press Ctrl + C for stop...")
-    a = float(input("input a number: "))
+    try:
+        a = float(input("input a number: "))
+    except ValueError:
+        print('Wrong input! Bye!')
+        os._exit(0)
     if a%2==0:
         print(a**2)
     elif a%2 !=0:
         print(a*2)
-    else:
-        print("Something wrong!")

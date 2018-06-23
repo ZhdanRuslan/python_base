a9974459bedc1d4778858523123871b7bf5d5430
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-06-23 16:16:42 +0300

    begin with console calc

---
+++ calc_proj/console_calc.py
@@ -0,0 +1,8 @@
+# simply console calculator
+a_var = float(input('Input first operand: '))
+action_var = input('Input an action e.q. "* / + -" : ')
+b_var = float(input('Input second operand: '))
+
+
+
+print(a_var, b_var)

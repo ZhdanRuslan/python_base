b3773b8b3345d05e6610c8ddf3df8a5945a52b42
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-06-23 16:26:46 +0300

    switch operations

--- calc_proj/console_calc.py
+++ calc_proj/console_calc.py
@@ -3,6 +3,20 @@ a_var = float(input('Input first operand: '))
 action_var = input('Input an action e.q. "* / + -" : ')
 b_var = float(input('Input second operand: '))
 
+#switch operation
+def switch_operation(arg):
+    if(arg=="*"):
+        return a_var * b_var
+    elif (arg=="/"):
+        return a_var / b_var
+    elif (arg=="+"):
+        return a_var + b_var
+    elif (arg=="-"):
+        return a_var - b_var
+    else:
+        print("wrong input")
 
 
-print(a_var, b_var)
+print(switch_operation(action_var))
+
+# print(a_var, b_var)

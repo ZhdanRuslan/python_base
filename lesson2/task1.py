43e4d8be47b9261a32fd7093785fd44500f36b1d
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-06-23 09:17:05 +0300

    binary converter

---
+++ lesson2/task1.py
@@ -0,0 +1,18 @@
+
+#Converter from dec to bin
+n = int(input('Input decimal number: '))
+
+#common logic of convertation
+def to_binary_converter(n):
+    k=[]
+    while (n>0):
+        a=int(float(n%2))
+        k.append(a)
+        n=(n-a)/2
+    k.append(0)
+    string=""
+    for j in k[::-1]:
+        string=string+str(j)
+    print('The binary num is %s'%(string))
+
+to_binary_converter(n)

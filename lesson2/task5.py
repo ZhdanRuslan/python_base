aedc27338265e0ac39e41d062ba136d66a813649
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-03 10:32:25 +0300

    add try except

--- lesson2/task5.py
+++ lesson2/task5.py
@@ -1,10 +1,15 @@
 print("Input the values: ")
-a = float(input("a: "))
-b = float(input("b: "))
-c = float(input("c: "))
+try:
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = float(input("c: "))
 
-x1 = (-b + (b * b - 4 * a * c) ** 0.5) / (2 * a)
-x2 = (-b - (b * b - 4 * a * c) ** 0.5) / (2 * a)
+    x1 = (-b + (b * b - 4 * a * c) ** 0.5) / (2 * a)
+    x2 = (-b - (b * b - 4 * a * c) ** 0.5) / (2 * a)
 
-print('result for x1 = ', x1)
-print('result for x2 = ', x2)
+except Exception as e:
+    print("Something wrong! Bye!" + e)
+
+else:
+    print('result for x1 = ', x1)
+    print('result for x2 = ', x2)

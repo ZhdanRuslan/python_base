2c26447270e0454dfda731c53dcd9509c2c6a932
Ruslan <zhd.ruslan@gmail.com>
2018-07-03 19:58:03 +0300

    class work

---
+++ lesson4/task1.py
@@ -0,0 +1,12 @@
+try:
+    n = 0
+    while n < 10:
+        name = str(input("Input name: "))
+        for z in name:
+            n += 1
+        if n < 10:
+            print("Name too short")
+    print("Everething OK")
+
+except Exception as e:
+    print("Something wrong!" + e)    

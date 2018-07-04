01ac239e500e36e84d50cb75e481b30561301e18
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-04 16:12:40 +0300

    add validation

--- lesson4/task4.py
+++ lesson4/task4.py
@@ -2,8 +2,13 @@ try:
     n1 = int(input("Input a: "))
     n2 = int(input("Input b: "))
 
-    sum = (n2*(n2+1))/2-((n1-1)*((n1-1)+1))/2
 except Exception as e:
-    print("Error!" + e)
+    print("Wrong input! Bye!")
 else:
-    print(sum)
+    while n1 >= n2:
+        print("n1 >= n2. Try again")
+        n1 = int(input("Input a: "))
+        n2 = int(input("Input b: "))
+    else:
+        sum = (n2*(n2+1))/2-((n1-1)*((n1-1)+1))/2
+        print(sum)

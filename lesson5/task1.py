0bf8c9d53bcc29f574104ca31b4be460e92eb6e7
Ruslan <zhd.ruslan@gmail.com>
2018-07-05 21:35:58 +0300

    task on lesson 5

---
+++ lesson5/task1.py
@@ -0,0 +1,15 @@
+try:
+    a = float(input("Input a: "))
+    b = float(input("Input b: "))
+except Exception as e:
+    print("Wrong input!" + e)
+else:
+    def outer_function(a, b):
+    
+        def pow_side(x):
+            return x*x
+
+        return pow_side(a) + pow_side(b)
+    
+
+print(outer_function(a,b))

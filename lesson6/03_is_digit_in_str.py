809d9008b246f82e395b79463953a841b798d579
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-12 15:30:52 +0300

    task3

---
+++ lesson6/03_is_digit_in_str.py
@@ -0,0 +1,33 @@
+import os
+
+def inserting_ind_into_str(input_string):
+    if not input_string or type(input_string) != str:
+        raise Exception
+    if is_digit_present(input_string) == None:
+        print("String have some digits! Bye!")
+        raise ValueError
+    if input_string.startswith(" "):
+        input_string = input_string.lstrip()
+    elif input_string.endswith(" "):
+        input_string = input_string.rstrip()
+    
+    result = ""
+    for index in range(len(input_string)):
+        result += input_string[index] + str(index)
+    return result
+
+# Check digit in string
+def is_digit_present(input_string):
+    if not input_string or type(input_string) != str:
+        raise Exception
+    for elem in input_string:
+        if elem.isdigit():
+            return None
+    else:
+        return input_string
+
+DATA = ["test string one", "ruslan", "  left spaces",
+"right spaces    ","   both side spaces   " ,"   both side spaces   "]
+
+for i in DATA:
+    print(inserting_ind_into_str(i))

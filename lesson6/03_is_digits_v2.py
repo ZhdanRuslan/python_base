dfafa563408e03e4c32bdb7f15b4beb96fa308e1
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-16 17:26:07 +0300

    ready

--- lesson6/03_is_digits_v2.py
+++ lesson6/03_is_digits_v2.py
@@ -1,13 +1,16 @@
 import re
 
 def inserting_ind_into_str(input_string):
-    if any(map(str.isdigit, input_string)):
+    if have_some_digits(input_string):
         return None
     input_string = input_string.strip()
     result = ""
     for index, elem in enumerate(input_string):
         result += elem + str(index)
     return result
+
+def have_some_digits(input_string):
+    return any(map(str.isdigit, input_string))
 
 str_with_dig = inserting_ind_into_str(" demo test    ")
 

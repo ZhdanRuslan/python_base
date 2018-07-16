78239d742d50101d98cadeb04ddde68f3e12b3f4
Ruslan <zhd.ruslan@gmail.com>
2018-07-16 22:33:53 +0300

    fix loop

--- lesson6/02_count_digits_str_v2.py
+++ lesson6/02_count_digits_str_v2.py
@@ -10,9 +10,9 @@ str_with_dig = inserting_ind_into_str("demo0234234234234 test")
 
 print(str_with_dig)
 
-res = re.findall("\d", str_with_dig)
+res = re.findall("\d+", str_with_dig)
 
 print(res)
 
-for ind in range(len(res)):
+for ind in res:
     print("{}-{}".format(ind, res.count(str(ind))))

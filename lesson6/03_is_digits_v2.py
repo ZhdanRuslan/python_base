e36defdbac75255beacd07c12b78d2823e7160e3
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-16 17:19:47 +0300

    digits v2'

---
+++ lesson6/03_is_digits_v2.py
@@ -0,0 +1,18 @@
+import re
+
+def inserting_ind_into_str(input_string):
+    if any(map(str.isdigit, input_string)):
+        return None
+    input_string = input_string.strip()
+    result = ""
+    for index, elem in enumerate(input_string):
+        result += elem + str(index)
+    return result
+
+str_with_dig = inserting_ind_into_str(" demo test    ")
+
+res = re.findall("\d+", str(str_with_dig))
+print(str_with_dig)
+
+for ind in range(len(res)):
+    print("{}-{}".format(ind, res.count(str(ind))))

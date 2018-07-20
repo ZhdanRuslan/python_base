dc78a6feaffe5d2adb1df319f745d43a55b790b1
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-20 17:33:12 +0300

    task 3

---
+++ lesson9/03_car_showroom_v1.py
@@ -0,0 +1,60 @@
+class Car:
+
+    def __init__(self, model, year, price, *Additional):
+        super().__init__()
+        self.model = model
+        self.year = year
+        self.price = price
+        self.Additional = Additional
+
+    def __str__(self):
+        return "[" + self.model + "] [" + self.year + "] [" + self.price + "] [" + str(self.Additional) + "]"
+
+    def __repr__(self):
+        return """class Car:
+    def __init__(self, model, year, price, *Additional):
+        super().__init__()
+        self.model = model
+        self.year = year
+        self.price = price
+        self.Additional = Additional"""
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+# -------------------------------------------------------
+class CarShowroom:
+    def __init__(self, *cars):
+        self.cars = list(cars)
+
+    def add_car(self, name):
+        self.cars.append(name)
+
+    def show_cars(self):
+        for car in self.cars:
+            print(car)
+
+    def sell(self, name):
+        self.cars.remove(name)
+        
+# --------------------------------------------------------
+
+bmw = Car("X3", "2016", "30000", "Full compl")
+bmw1 = Car("X5", "2017", "50000", "Full compl+")
+
+shop = CarShowroom(bmw, bmw1)
+shop.show_cars()
+print("--------------------------------------")
+mazda = Car("3", "2015", "15000", "Base compl")
+shop.add_car(mazda)
+shop.show_cars()
+print("--------------------------------------")
+shop.sell(bmw)
+shop.show_cars()
+print("--------------------------------------")

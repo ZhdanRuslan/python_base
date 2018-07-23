95e8209ed60aeff24a31160723602da8c71d966d
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-07-23 13:26:03 +0300

    add cache and comments

--- calc_proj/calc.py
+++ calc_proj/calc.py
@@ -1,21 +1,30 @@
 import logging
 import os.path
+from functools import lru_cache
 
 logging.basicConfig(filename = os.path.join("calc_proj", "logfile.log"),level=logging.INFO, format = u'%(levelname)-8s [%(asctime)s] %(message)s')
 
+@lru_cache(maxsize = 32)
 def add (a, b):
+    """Return the sum of a and b"""
     logging.info(str(a) + " + " + str(b) + " was called.")
     return a + b
 
+@lru_cache(maxsize = 32)
 def sub (a, b):
+    """Return the sub of a and b"""
     logging.info(str(a) + " - " + str(b) + " was called.")
     return a - b
 
+@lru_cache(maxsize = 32)
 def mul (a, b):
+    """Return the mul of a and b"""
     logging.info(str(a) + " * " + str(b) + " was called.")
     return a * b
 
+@lru_cache(maxsize = 32)
 def div (a, b):
+    """Return the div of a and b"""
     if b == 0:
         logging.info("Dev by 0 was occured.")
         raise ZeroDivisionError
@@ -24,7 +33,8 @@ def div (a, b):
         return a / b
 
 #switch operation
-def switch_operation(arg):
+def switch_operation(arg, a_var, b_var):
+    """Choose an operation which apply for a and b"""
     if(arg=="*"):
         return mul(a_var, b_var)
     elif (arg=="/"):
@@ -36,7 +46,9 @@ def switch_operation(arg):
     else:
         logging.info("Invalid arithmetic operator in input line!")
         raise Exception
+
 def main():
+    """Start point of a programm"""
     action_var = ""
     while True:
         try:
@@ -51,7 +63,7 @@ def main():
         except ValueError as e:
             logging.info("Invalid value!")   
         try:
-            print(switch_operation(action_var))
+            print(switch_operation(action_var, a_var, b_var))
         except Exception as e:
             print("Somethings wrong!", e)
 
--- calc_proj/calc_test.py
+++ calc_proj/calc_test.py
@@ -2,17 +2,22 @@ import pytest
 from calc import (add, sub, mul, div)
 
 def test_sum():
+    """Testing sum function"""
     assert add(2, 5) == 7
 
 def test_sub():
+    """Testing sub function"""
     assert sub(2, 5) == -3
 
 def test_mul():
+    """Testing mul function"""
     assert mul(2, 5) == 10
 
 def test_div():
+    """Testing div function"""
     assert div(6, 3) == 2
 
 def test_div_by_zero():
+    """Testing div function with div by 0 case"""
     with pytest.raises(ZeroDivisionError):
         div(6, 0)
